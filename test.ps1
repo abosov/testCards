79a4497979ebdbb8ebea5cde21f9d5ff5505f009
Alexander Bosov <Bosov@avp.ru>
2018-08-30 16:31:11 +0300

    error1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,16 @@
-Invoke-Expression ".\cards.ps1 -test"
+$error1 = $(Invoke-Expression ".\cards.ps1 -test")
 
-Write-Host "END TEST! (test.ps1)"
+try {
+    Invoke-Expression ".\cards.ps1 -test"
+} catch {
+   exit 123 # error handling go here, $_ contains the error record
+}
+
+Write-Host "11111: $?"
+
+if ($error1 -eq $true){
+    Write-Host "Error: $error1"
+    exit $error1
+}
+
+Write-Host "------END TEST! (test.ps1)"
